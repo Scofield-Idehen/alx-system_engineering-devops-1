75ecdb98c75dcf06d9d3062e9ddaaf68b70703d0
sandysode <sandraoghenessode@gmail.com>
2024-01-04 05:41:48 -0800

    ssh

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+# Automated my Tasks using Puppet
+
+file { '/etc/ssh/ssh_config':
+  ensure  => present,
+content => "
+    # SSH client configuration
+    Host *
+      IdentityFile ~/.ssh/school
+      PasswordAuthentication no
+  ",
+}
