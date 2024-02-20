d9af2aee8df80d636d0bd5d1fc9896a586285ad9
sandysode <sandraoghenessode@gmail.com>
2024-02-20 07:17:06 -0800

    debugg

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,8 @@
+# 0. Sky is the limit, let's bring that limit higher
+exec { 'set limit to 2000':
+  path    => '/bin',
+  command => "sed -i 's/15/2000/' /etc/default/nginx"
+}
+exec { 'reboot nginx':
+  command => '/usr/sbin/service nginx restart'
+}
